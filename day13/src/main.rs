101f6e45dd44c0ce6779e91596b8e47113068e2a
Staszek Pasko <paskos@amazon.com>
2019-12-14 09:15:26 +0100

    day 13

---
+++ day13/src/main.rs
@@ -0,0 +1,208 @@
+extern crate pancurses;
+
+use std::io;
+use pancurses::Input;
+
+struct IntCode {
+    prog : Vec<i64>,
+    input : Vec<i64>,
+    pos : usize,
+    ipos : usize,
+    ccnt : usize,
+    relbase: i64,
+}
+
+struct Op3 {
+    v1: i64,
+    v2: i64,
+    v3: i64
+}
+
+fn read3(v : &Vec<i64>, mode: i64, ic: &IntCode) -> Op3 {
+    let mut op = Op3 { v1: 0, v2: 0, v3: 0 };
+    let mode1 = mode % 10;
+    let mode2 = (mode / 10) % 10;
+    let mode3 = (mode / 100) % 10;
+    op.v1 = v[ic.pos+1]; 
+    op.v2 = v[ic.pos+2];
+    op.v3 = v[ic.pos+3];
+    //println!("@{} opcode {} mode {} : {}, {}, {}", pos, v[pos], mode, op.v1, op.v2, op.v3);
+    if mode1 == 0 { op.v1 = v[op.v1 as usize]; }
+    if mode1 == 2 { op.v1 = v[(op.v1 + ic.relbase) as usize]; }
+    if mode2 == 0 { op.v2 = v[op.v2 as usize]; }
+    if mode2 == 2 { op.v2 = v[(op.v2 + ic.relbase) as usize]; }
+    if mode3 == 2 { op.v3 += ic.relbase; }
+    return op;
+} 
+
+struct Op2 {
+    v1: i64,
+    v2: i64
+}
+
+fn read2(v : &Vec<i64>, mode: i64, ic: &IntCode) -> Op2 {
+    let mut op = Op2 { v1: 0, v2: 0 };
+    let mode1 = mode % 10;
+    let mode2 = (mode / 10) % 10;
+    op.v1 = v[ic.pos+1]; 
+    op.v2 = v[ic.pos+2];
+    //println!("@{} opcode {} mode {} : {}, {}", pos, v[pos], mode, op.v1, op.v2);
+    if mode1 == 0 { op.v1 = v[op.v1 as usize]; }
+    if mode1 == 2 { op.v1 = v[(op.v1 + ic.relbase) as usize]; }
+    if mode2 == 0 { op.v2 = v[op.v2 as usize]; }
+    if mode2 == 2 { op.v2 = v[(op.v2 + ic.relbase) as usize]; }
+    return op;
+} 
+
+fn read1(v : &Vec<i64>, mode: i64, ic: &IntCode) -> i64 {
+    let mut v1 = v[ic.pos+1]; 
+    //println!("@{} opcode {} mode {} : {}", pos, v[pos], mode, v1);
+    if mode == 0 { v1 = v[v1 as usize]; }
+    if mode == 2 { v1 = v[(v1 + ic.relbase) as usize]; }
+    return v1;
+} 
+
+fn run_program(ic: & mut IntCode) -> Option<i64> {
+    loop {
+        ic.ccnt += 1;
+        let opcode = ic.prog[ic.pos] % 100;
+        let mode = ic.prog[ic.pos] / 100;
+        match opcode {
+            1 => { 
+                    let op = read3(&ic.prog, mode, ic);
+                    ic.prog[op.v3 as usize]= op.v1 + op.v2;
+                    //println!("m[{}] = {} + {} => {}",op.v3,op.v1,op.v2,prog[op.v3 as usize]);
+                    ic.pos += 4;
+                }, 
+            2 => { 
+                    let op = read3(&ic.prog, mode, ic);
+                    ic.prog[op.v3 as usize] = op.v1 * op.v2;
+                    //println!("m[{}] = {} * {} => {}",op.v3,op.v1,op.v2,prog[op.v3 as usize]);
+                    ic.pos += 4;
+                }, 
+            3 => { 
+                    if ic.ipos >= ic.input.len() {
+                        return None;
+                    }
+                    let mut v = ic.prog[ic.pos + 1]; 
+                    if mode == 2 { v += ic.relbase; }
+                    ic.prog[v as usize] = ic.input[ic.ipos];
+                    //println!("Input: {} => {} (mode {})", v, ic.prog[v as usize], mode);
+                    ic.ipos += 1;
+                    ic.pos += 2;
+                 }, 
+            4 => { 
+                    let v = read1(&ic.prog, mode, ic); 
+                    //println!("Output: {}", v);
+                    ic.pos += 2;
+                    return Some(v);
+                 }, 
+            5 => {  
+                    let op = read2(&ic.prog, mode, ic); 
+                    ic.pos += 3;
+                    if op.v1 != 0 { ic.pos = op.v2 as usize; }
+                    //println!("@ = {}? => {}",op.v1,op.v2);
+                 }, 
+            6 => {  
+                    let op = read2(&ic.prog, mode, ic); 
+                    ic.pos += 3;
+                    if op.v1 == 0 { ic.pos = op.v2 as usize; }
+                    //println!("@ = !{}? => {}",op.v1,op.v2);
+                  }, 
+            7 => {  
+                    let op = read3(&ic.prog, mode, ic); 
+                    ic.prog[op.v3 as usize] = if op.v1 < op.v2 { 1 } else { 0 };
+                    //println!("m[{}] = {} < {} => {}",op.v3,op.v1,op.v2,prog[op.v3 as usize]);
+                    ic.pos += 4;
+                 }, 
+            8 => {  
+                    let op = read3(&ic.prog, mode, ic); 
+                    ic.prog[op.v3 as usize] = if op.v1 == op.v2 { 1 } else { 0 };
+                    //println!("m[{}] = {} == {} => {}",op.v3,op.v1,op.v2,prog[op.v3 as usize]);
+                    ic.pos += 4;
+                  }, 
+            9 => { 
+                    let v = read1(&ic.prog, mode, ic); 
+                    ic.relbase += v;
+                    ic.pos += 2;
+                 }, 
+            99 => break,
+            _ => panic!("Failed"),
+        }
+    }
+    return None;
+}
+
+fn main() {
+    let mut buf = String::new();
+    io::stdin().read_line(&mut buf).expect("Failure");
+    let split = buf.trim().split(",");
+    let mut memory : Vec<_> = split.filter_map(|x| x.parse::<i64>().ok()).collect();
+    memory.resize(10000000, 0);
+    let mut m = IntCode { prog: memory.to_vec(), input: vec![ 0 ], pos: 0, ipos: 0, ccnt: 0, relbase: 0 };
+    // update!
+    m.prog[0] = 2;
+    let window = pancurses::initscr();
+    window.mv(window.get_max_y() - 1, window.get_max_x() - 1);
+    pancurses::nl();
+    pancurses::noecho();
+    pancurses::curs_set(0);
+    window.timeout(0);
+    window.keypad(true);
+    window.nodelay(true);
+    let pc = vec![b'/', b'-', b'\\', b'|'];
+    let mut pp = 0;
+    let mut pb = 0;
+    let mut pr = 0;
+    loop {
+        let mut dir = 0;
+        if pb > pr { dir = 1; }
+        if pb < pr { dir = -1; }
+        window.refresh();
+        match window.getch() {
+            Some(Input::Character(q)) if q == 'q' || q == 'Q' => {
+                pancurses::curs_set(1);
+                pancurses::endwin();
+                return;
+            }
+            Some(Input::Character('w')) => {
+                dir = -1;
+            }
+            Some(Input::Character('e')) => {
+                dir = 1;
+            }
+            Some(Input::Character('s')) => {
+                window.nodelay(false);
+            }
+            Some(Input::Character(' ')) => {
+                window.nodelay(true);
+            }
+            _ => {}
+        }
+        pancurses::napms(10);
+        m.ipos = 0;
+        m.input[0] = dir;
+        loop {
+            let x = run_program(&mut m);
+            let y = run_program(&mut m);
+            let z = run_program(&mut m);
+            if x.is_none() || y.is_none() || z.is_none() { break; }
+            if x.unwrap() < 0 && y.unwrap() == 0 {
+                window.mvaddstr(30, 0, format!("Score: {}", z.unwrap()));
+            } else {
+                let bch = match z.unwrap() {
+                    0 => ' ',
+                    1 => '#',
+                    2 => '@',
+                    3 => { pr = x.unwrap(); '=' }
+                    4 => { pb = x.unwrap(); '*' }
+                    _ => '?'
+                };  
+                window.mvaddch(y.unwrap() as i32, x.unwrap() as i32, bch);  
+            }
+        }
+        window.mvaddch(40, 40, pc[pp] as char);
+        pp = (pp + 1) % pc.len();
+    }
+    // println!("Total: {}",tot)
+}
